0dd313814c608621a903605c86bbca2ef8db7782
Johannes Kettmann <jkettmann@gmx.net>
2022-03-10 13:20:54 +0100

    Formats files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+};
