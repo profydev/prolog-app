171dd1a0f647f664c916aa3a196ed86aa8e7e1c2
Johannes Kettmann <jkettmann@gmx.net>
2022-04-04 10:13:57 +0200

    Adds normalize.css

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
 
 export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  
   @font-face {
     font-family: "Inter";
     src: url("/fonts/Inter.ttf");
