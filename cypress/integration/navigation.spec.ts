388732106944c6d58881d3efe1b58a4e983e87e7
Johannes Kettmann <jkettmann@gmx.net>
2022-04-07 12:01:45 +0200

    Adds test for mobile menu

--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -11,10 +11,57 @@ describe("Sidebar Navigation", () => {
     cy.url().should("eq", "http://localhost:3000/");
   });
 
-  it("is collapsible", () => {
-    cy.findByText("Collapse").click();
-    cy.findByRole("navigation").findAllByRole("link").should("have.length", 5);
-    cy.findByRole("navigation").findByText("Issues").should("not.exist");
+  context("desktop resolution", () => {
+    it("is collapsible", () => {
+      cy.findByText("Collapse").click();
+      cy.findByRole("navigation")
+        .findAllByRole("link")
+        .should("have.length", 5);
+      cy.findByRole("navigation").findByText("Issues").should("not.exist");
+    });
+  });
+
+  context("mobile resolution", () => {
+    beforeEach(() => {
+      cy.viewport("iphone-8");
+    });
+
+    function isInViewport(el: string) {
+      cy.get(el).then(($el) => {
+        const rect = $el[0].getBoundingClientRect();
+        expect(rect.right).to.be.equal(rect.width);
+        expect(rect.left).to.be.equal(0);
+      });
+    }
+
+    function isNotInViewport(el: string) {
+      cy.get(el).then(($el) => {
+        const rect = $el[0].getBoundingClientRect();
+        expect(rect.left).to.be.equal(-rect.width);
+        expect(rect.right).to.be.equal(0);
+      });
+    }
+
+    it("toggles sidebar navigation by clicking the menu icon", () => {
+      cy.wait(500);
+      isNotInViewport("nav");
+
+      cy.findByAltText("open menu").click();
+
+      cy.wait(500);
+      isInViewport("nav");
+
+      cy.findByRole("navigation")
+        .findAllByRole("link")
+        .should("have.length", 5);
+
+      cy.findByText("Support").should("exist");
+      cy.findByText("Collapse").should("not.be.visible");
+
+      cy.findByAltText("close menu").click();
+      cy.wait(500);
+      isNotInViewport("nav");
+    });
   });
 });
 
