85f3c685964c4fc3d87ffcf8d61019b70f10b2ca
Johannes Kettmann <jkettmann@gmx.net>
2022-04-11 12:05:53 +0200

    Creates inner content container

--- components/PageContainer/PageContainer.tsx
+++ components/PageContainer/PageContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import styled from "styled-components";
 import { SidebarNavigation } from "@components/SidebarNavigation";
-import { color } from "@styles/theme";
+import { color, space } from "@styles/theme";
 
 type PageContainerProps = {
   children: React.ReactNode;
@@ -11,6 +11,18 @@ type PageContainerProps = {
 const Container = styled.div`
   display: flex;
   background: ${color("gray", 900)};
+`;
+
+const Main = styled.main`
+  flex: 1;
+`;
+
+const ContentContainer = styled.div`
+  min-height: calc(100vh - ${space(3)});
+  margin-top: ${space(3)};
+  padding: ${space(8)};
+  background: white;
+  border-top-left-radius: ${space(10)};
 `;
 
 export function PageContainer({ children }: PageContainerProps) {
@@ -23,7 +35,9 @@ export function PageContainer({ children }: PageContainerProps) {
       </Head>
 
       <SidebarNavigation />
-      <main>{children}</main>
+      <Main>
+        <ContentContainer>{children}</ContentContainer>
+      </Main>
     </Container>
   );
 }
