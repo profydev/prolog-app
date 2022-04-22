376107c87a5d22db7c6c09b7294069fd48068fa5
Johannes <jkettmann@gmx.net>
2022-04-22 09:18:24 +0200

    Fix font weights (#16)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { QueryClient, QueryClientProvider } from "react-query";
--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -5,12 +5,6 @@ import { color } from "./theme";
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
   
-  @font-face {
-    font-family: "Inter-md";
-    src: url("/fonts/Inter.ttf");
-    font-display: swap;
-  }
-
   body {
     font-family: Inter, Sans-Serif;
     color: ${color("gray", 900)};
