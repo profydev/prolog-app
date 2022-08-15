0d32f0f7e30e8acb172d9fef260d50a470600bc9
Johannes Kettmann <jkettmann@gmx.net>
2022-08-15 08:42:33 +0200

    Add missing pages

---
+++ pages/settings.tsx
@@ -0,0 +1,12 @@
+import { PageContainer } from "@features/ui";
+import type { NextPage } from "next";
+
+const IssuesPage: NextPage = () => {
+  return (
+    <PageContainer title="Settings" info="The account settings">
+      This page has yet to be implemented
+    </PageContainer>
+  );
+};
+
+export default IssuesPage;
