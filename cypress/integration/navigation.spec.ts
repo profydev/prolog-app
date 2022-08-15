28ff6da766b170e224f74ca1a918bef26c56802e
Johannes Kettmann <jkettmann@gmx.net>
2022-08-15 08:42:55 +0200

    Test navigation to new pages

--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -16,6 +16,18 @@ describe("Sidebar Navigation", () => {
       // check that projects link leads to the right page
       cy.get("nav").contains("Projects").click();
       cy.url().should("eq", "http://localhost:3000/");
+
+      // check that issues link leads to the right page
+      cy.get("nav").contains("Alerts").click();
+      cy.url().should("eq", "http://localhost:3000/alerts");
+
+      // check that issues link leads to the right page
+      cy.get("nav").contains("Users").click();
+      cy.url().should("eq", "http://localhost:3000/users");
+
+      // check that issues link leads to the right page
+      cy.get("nav").contains("Settings").click();
+      cy.url().should("eq", "http://localhost:3000/settings");
     });
 
     it("is collapsible", () => {
