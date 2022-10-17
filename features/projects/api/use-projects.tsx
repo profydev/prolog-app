335e140abc2fb6c6566f035bf49580be7ba85d77
Johannes Kettmann <jkettmann@gmx.net>
2022-10-17 12:21:35 +0200

    Extract api functions

--- features/projects/api/use-projects.tsx
+++ features/projects/api/use-projects.tsx
@@ -1,11 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { axios } from "@api/axios";
+import { getProjects } from "@api/projects";
 import type { Project } from "@features/projects";
-
-async function getProjects() {
-  const { data } = await axios.get("/project");
-  return data;
-}
 
 export function useProjects() {
   return useQuery<Project[], Error>(["projects"], getProjects);
