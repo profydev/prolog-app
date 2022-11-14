691a879b5a66d91169b22ef6d9111d00253f2a29
Johannes Kettmann <jkettmann@gmx.net>
2022-11-14 13:26:20 +0100

    Migrate to Cypress 11

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,4 +5,10 @@ export default defineConfig({
     runMode: 2,
     openMode: 1,
   },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
