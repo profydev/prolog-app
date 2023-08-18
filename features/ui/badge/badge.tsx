5e5df0e063deb5660690cd84e92e3c7e407afad7
Johannes Kettmann <jkettmann@gmx.net>
2023-08-18 16:15:33 +0200

    Migrate badge

--- features/ui/badge/badge.tsx
+++ features/ui/badge/badge.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styled, { css } from "styled-components";
-import { color, textFont, space } from "@styles/theme";
+import classNames from "classnames";
+import styles from "./badge.module.scss";
 
 export enum BadgeSize {
   sm = "sm",
@@ -22,59 +22,14 @@ type BadgeProps = {
   color?: BadgeColor;
 };
 
-const Container = styled.div<{ size: BadgeSize; color: BadgeColor }>`
-  width: fit-content;
-  display: flex;
-  align-items: center;
-  border-radius: ${space(4)};
-
-  ${(props) => {
-    switch (props.size) {
-      case BadgeSize.sm:
-        return css`
-          padding: ${space(0, 2)};
-          height: 1.375rem;
-          ${textFont("xs", "medium")}
-        `;
-      case BadgeSize.md:
-        return css`
-          padding: 0 0.625rem;
-          height: 1.5rem;
-          ${textFont("sm", "medium")}
-        `;
-      case BadgeSize.lg:
-        return css`
-          padding: ${space(0, 3)};
-          height: 1.75rem;
-          ${textFont("sm", "medium")}
-        `;
-    }
-  }}
-
-  ${(props) => {
-    switch (props.color) {
-      case BadgeColor.gray:
-        return css`
-          background: ${color("gray", 100)};
-          color: ${color("gray", 700)};
-        `;
-      default:
-        return css`
-          background: ${color(props.color, 50)};
-          color: ${color(props.color, 700)};
-        `;
-    }
-  }}
-`;
-
 export function Badge({
   children,
   size = BadgeSize.md,
   color = BadgeColor.primary,
 }: BadgeProps) {
   return (
-    <Container size={size} color={color}>
+    <div className={classNames(styles.container, styles[size], styles[color])}>
       {children}
-    </Container>
+    </div>
   );
 }
