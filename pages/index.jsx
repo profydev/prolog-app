a51f76f451485cd702115474a960273fe1ed2917
Johannes Kettmann <jkettmann@gmx.net>
2023-08-18 16:23:25 +0200

    Migrate index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,42 +1,16 @@
-import styled from "styled-components";
 import { Routes } from "@config/routes";
-
-const Header = styled.header`
-  width: 100%;
-  height: 80px;
-  padding: 0 2rem;
-  box-sizing: border-box;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background: white;
-`;
-
-const ContactButton = styled.button`
-  position: absolute;
-  bottom: 2.5rem;
-  right: 2.5rem;
-  padding: 1rem;
-  background: #7f56d9;
-  border-radius: 50%;
-  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
-  border: none;
-  cursor: pointer;
-
-  &:hover {
-    background: #6941c6;
-  }
-`;
+import styles from "./index.module.scss";
 
 const IssuesPage = () => {
   return (
     <div>
-      <Header>
+      <header className={styles.header}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
         <a href={Routes.projects}>Dashboard</a>
-      </Header>
-      <ContactButton
+      </header>
+      <button
+        className={styles.contactButton}
         onClick={() =>
           alert(
             "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
@@ -45,7 +19,7 @@ const IssuesPage = () => {
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/message.svg" alt="Contact" />
-      </ContactButton>
+      </button>
     </div>
   );
 };
