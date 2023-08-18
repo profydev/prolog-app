816cae9074d322fd2089f9fb6fb99fae06cef36c
Johannes Kettmann <jkettmann@gmx.net>
2023-08-18 16:19:40 +0200

    Migrate button

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,20 +1,9 @@
-import styled from "styled-components";
+import { ButtonHTMLAttributes } from "react";
+import classNames from "classnames";
+import styles from "./button.module.scss";
 
-export const Button = styled.button`
-  cursor: pointer;
-
-  // remove default button styles
-  border: none;
-  margin: 0;
-  padding: 0;
-  background: transparent;
-  line-height: normal;
-  -webkit-font-smoothing: inherit;
-  -moz-osx-font-smoothing: inherit;
-  -webkit-appearance: none;
-
-  &::-moz-focus-inner {
-    border: 0;
-    padding: 0;
-  }
-`;
+export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button {...props} className={classNames(styles.button, props.className)} />
+  );
+}
