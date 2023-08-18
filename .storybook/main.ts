5938fefaa29aba56cbfdf5c7944570802bc90141
Johannes Kettmann <jkettmann@gmx.net>
2023-08-18 09:38:46 +0200

    Upgrade Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,31 @@
 const path = require("path");
 
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
   stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react",
+
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+
   staticDirs: ["../public"],
+
   env: (config) => ({
     ...config,
     // ensure that new next/link is enabled in storybook
     // required for sidebar navigation stories
     __NEXT_NEW_LINK_BEHAVIOR: true,
   }),
+
   webpackFinal: async (config) => {
     config.resolve.alias["next/router"] = require.resolve(
-      "../__mocks__/next/router.tsx"
+      "../__mocks__/next/router.tsx",
     );
     config.resolve.alias["@api"] = path.resolve("./api");
     config.resolve.alias["@config"] = path.resolve("./config");
